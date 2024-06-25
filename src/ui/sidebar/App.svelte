83bf139d274fab8893c66460ae4020a95cc5e730
codewithcheese <mr.tmanning@gmail.com>
2024-06-25 21:01:46 +1000

    Add shadcn theme

--- src/ui/sidebar/App.svelte
+++ src/ui/sidebar/App.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
   import "../app.css";
   import { Button } from "$ui/components/ui/button";
-
-  let count = $state(0);
-  function increment() {
-    count += 1;
-  }
-
-  console.log("Hello from svelte!");
 </script>
 
-<main>
-  <h1>Codewithcheese</h1>
-  <Button variant="outline" onclick={increment}>
-    Clicks: {count}
-  </Button>
+<main class="p-4">
+  <Button variant="outline">New chat</Button>
 </main>

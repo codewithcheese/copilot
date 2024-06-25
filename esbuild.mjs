5027550e6000378a02aaf7dc3e1b08aef784863d
codewithcheese <mr.tmanning@gmail.com>
2024-06-25 19:57:33 +1000

    Add shadcn-svelte

--- esbuild.mjs
+++ esbuild.mjs
@@ -4,6 +4,13 @@ import {sveltePreprocess} from 'svelte-preprocess';
 import stylePlugin from 'esbuild-style-plugin';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import * as path from "node:path";
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const production = process.argv.includes('--production');
 const watch = process.argv.includes('--watch');
@@ -23,6 +30,18 @@ const esbuildProblemMatcherPlugin = {
         });
     },
 };
+
+const aliasPlugin = (aliases) => ({
+    name: 'alias',
+    setup(build) {
+        Object.entries(aliases).forEach(([alias, aliasPath]) => {
+            build.onResolve({ filter: new RegExp(`^${alias}`) }, args => {
+                const resolvedPath = path.join(aliasPath, args.path.slice(alias.length));
+                return { path: resolvedPath };
+            });
+        });
+    }
+});
 
 async function buildExtension() {
     const ctx = await esbuild.context({
@@ -47,7 +66,7 @@ async function buildExtension() {
     }
 }
 
-async function buildWebview() {
+async function buildUI() {
     const ctx = await esbuild.context({
         entryPoints: [
             'src/ui/sidebar/index.ts',
@@ -58,7 +77,11 @@ async function buildWebview() {
         minify: production,
         sourcemap: !production,
         outdir: 'dist/ui',
+        conditions: ['svelte', 'browser', 'import'],
         plugins: [
+            aliasPlugin({
+                '$ui': path.resolve(__dirname, 'src/ui'),
+            }),
             esbuildSvelte({
                 preprocess: sveltePreprocess(),
             }),
@@ -86,7 +109,7 @@ async function buildWebview() {
 
 async function main() {
     try {
-        await Promise.all([buildExtension(), buildWebview()]);
+        await Promise.all([buildExtension(), buildUI()]);
     } catch (e) {
         console.error(e);
         process.exit(1);
--- src/ui/sidebar/App.svelte
+++ src/ui/sidebar/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import "../app.css";
+  import { Button } from "$ui/components/ui/button";
 
   let count = $state(0);
   function increment() {
@@ -11,30 +12,7 @@
 
 <main>
   <h1>Codewithcheese</h1>
-  <button onclick={increment}>
+  <Button variant="outline" onclick={increment}>
     Clicks: {count}
-  </button>
+  </Button>
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  button {
-    font-size: 2em;
-    padding: 0.5em 1em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
-    border: 2px solid #ff3e00;
-    cursor: pointer;
-  }
-</style>
